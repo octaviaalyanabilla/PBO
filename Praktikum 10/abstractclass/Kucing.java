c4e53a8ca982b52625a6b942f4c72296a41655a7
ACER\Z1402 <octaviaalya01@gmail.com>
2020-11-02 16:27:55 +0700

    Commit Keduabelas

---
+++ Praktikum 10/abstractclass/Kucing.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboprak10.abstractclass;
+
+/**
+ *
+ * @author Z1402
+ */
+public class Kucing extends Hewan{
+    
+    @Override
+    public void bergerak(){
+        System.out.println("Berjalan dengan KAKI, \"Tap..tap..\"");
+    }
+}
